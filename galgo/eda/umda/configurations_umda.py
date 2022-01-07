66337cc671292ec6c3c2e0f207fdb6f5be1440bf
victorperezpiqueras <victorperezpiqueras@gmail.com>
2022-01-07 13:55:01 +0100

    add umda configs to executer and algorithm and add new configs for run

--- galgo/eda/umda/configurations_umda.py
+++ galgo/eda/umda/configurations_umda.py
@@ -1,29 +1,40 @@
 def generate_configurations():
-    # eda umda s1 5 100 300 50
+    # eda umda s1 5 100 300 50 nds replacement
     # opciones------------------------------------------------------------------
     dataset_problems = [
         "1",
         "2",
-        "s1",
-        "s2",
+        # "s1",
+        # "s2",
         "s3",
     ]
     algorithms = [
         "umda"
     ]
     population_length = [
-        #20, 50, 
-        100, 200, 500
+        # 20,
+        50,
+        100, 200,  # 500
     ]
     max_generations = [
-        100, 200, 500
+        # 20,
+        50,
+        100, 200,  # 500
     ]
     evaluations = [
-        10000,
+        # 10000,
         0
     ]
     selected_individuals = [
-        20
+        20, 50
+    ]
+    selection_schemes = [
+        "nds",
+        # "monoscore"
+    ]
+    replacement_schemes = [
+        "replacement",
+        "elitism"
     ]
     seed = 10
 
@@ -36,12 +47,16 @@ def generate_configurations():
                 for max_generation in max_generations:
                     for evaluation in evaluations:
                         for selected_individual in selected_individuals:
-                            returnStr = type + ' ' + str(algorithm) + " " + str(dataset_problem) + ' ' + \
-                                str(seed) + ' ' + str(pop) + ' ' + \
-                                str(max_generation) + ' ' + \
-                                str(evaluation) + ' ' + \
-                                str(selected_individual) + '\n'
-                            f.write(returnStr)
+                            for selection_scheme in selection_schemes:
+                                for replacement_scheme in replacement_schemes:
+                                    returnStr = type + ' ' + str(algorithm) + " " + str(dataset_problem) + ' ' + \
+                                        str(seed) + ' ' + str(pop) + ' ' + \
+                                        str(max_generation) + ' ' + \
+                                        str(evaluation) + ' ' + \
+                                        str(selected_individual) + ' ' + \
+                                        str(selection_scheme) + ' ' + \
+                                        str(replacement_scheme) + '\n'
+                                    f.write(returnStr)
     f.close()
 
 
