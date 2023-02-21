a7a33989205a8c7605744d28d9a5d5406d66ef1c
victorperezpiqueras <victorperezpiqueras@gmail.com>
2023-02-21 18:36:43 +0100

    added test json and main for combinedataset script

--- datasets/utils/requirements_combination/combine_dataset_requirements.py
+++ datasets/utils/requirements_combination/combine_dataset_requirements.py
@@ -1,12 +1,24 @@
+import json
+import sys
 from typing import Dict
 
 
-def combine_dataset_requirements(json_data: dict) -> dict:
+def combine_dataset_requirements(json_data: dict, log_info: bool = False) -> dict:
     """
-    Combines requirements that have dependencies between them. 
-    For example, given a set of requirements { r1, r2, r3, r4 } and the following dependencies:
-    r1 -> r2; r2 -> r1; the resulting requirements set will be { r12, r3, r4 }, where r12 is the combination of r1 and r2.
+    Combines requirements that have interaction dependencies of type <combination> between them. 
+
+    For example, given a set of requirements { r1, r2, r3, r4 } and the following combination dependency:
+    r1 <--> r2 (coded as r1 -> r2; r2 -> r1); the resulting requirements set will be { r1+r2, r3, r4 }, where r1+r2 is the combination of r1 and r2.
     """
+    if "dependencies" not in json_data or json_data["dependencies"] is None:
+        raise Exception("No dependencies found in dataset")
+    if "pbis_cost" not in json_data or json_data["pbis_cost"] is None:
+        raise Exception("No pbis_cost found in dataset")
+    if "stakeholders_importances" not in json_data or json_data["stakeholders_importances"] is None:
+        raise Exception("No stakeholders_importances found in dataset")
+    if "stakeholders_pbis_priorities" not in json_data or json_data["stakeholders_pbis_priorities"] is None:
+        raise Exception("No stakeholders_pbis_priorities found in dataset")
+
     combined_requirements = dict()
     # for each requirement, check its dependencies
     for req_index in range(len(json_data["dependencies"])):
@@ -81,7 +93,7 @@ def combine_dataset_requirements(json_data: dict) -> dict:
             final_requirements_combined_index = next(
                 (i for i, v in enumerate(final_requirements_combined) if index in v), None)
             new_index = len(new_data["pbis_cost"]) + \
-                        final_requirements_combined_index
+                final_requirements_combined_index
             # get final_requirements_combined index where the current index is located
             new_indexes[index] = new_index
 
@@ -111,15 +123,28 @@ def combine_dataset_requirements(json_data: dict) -> dict:
 
         comb_requirements_dependencies = []
         for index in combined_requirements_set:
-            comb_requirements_dependencies.append(json_data["dependencies"][index])
+            comb_requirements_dependencies.append(
+                json_data["dependencies"][index])
             if comb_requirements_dependencies[-1]:
                 comb_requirements_dependencies[-1] = [new_indexes[dep] for dep in comb_requirements_dependencies[-1]
                                                       if dep not in combined_requirements_set]
         # flatten comb_requirements_dependencies
-        comb_requirements_dependencies = [item for sublist in comb_requirements_dependencies for item in sublist]
-        new_data["dependencies"].append(list(set(comb_requirements_dependencies)))
+        comb_requirements_dependencies = [
+            item for sublist in comb_requirements_dependencies for item in sublist]
+        new_data["dependencies"].append(
+            list(set(comb_requirements_dependencies)))
         if not new_data["dependencies"][-1]:
             new_data["dependencies"][-1] = None
+
+    if log_info:
+        print("Previous number of requirements: {}".format(
+            len(json_data["pbis_cost"])))
+        print("New number of requirements: {}".format(
+            len(new_data["pbis_cost"])))
+        print("Number of combination dependencies found: {}".format(
+            len(final_requirements_combined)))
+        print(
+            f"Requirement combinations: {[', '.join([f'r_{index}' for index in combination]) for combination in final_requirements_combined]}")
 
     return new_data
 
@@ -128,3 +153,23 @@ def is_requirement_affected_by_dependency(req_index: int, other_dependency_index
     # find if there is a dependency between the two requirements
     return json_data["dependencies"][other_dependency_index] is not None and \
         req_index in json_data["dependencies"][other_dependency_index]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        json_filename = sys.argv[1]
+    else:
+        raise Exception(
+            "Invalid number of arguments. Usage: python3 combine_dataset_requirements.py <input_json_filename>")
+
+    with open(json_filename, "r") as json_file:
+        json_data = json.load(json_file)
+
+    new_data = combine_dataset_requirements(json_data, log_info=True)
+
+    output_filename = json_filename.replace(".json", "_combined.json")
+    with open(output_filename, "w") as json_file:
+        json.dump(new_data, json_file)
+
+    print("Dataset requirements successfully combined. Output file: {}".format(
+        output_filename))
